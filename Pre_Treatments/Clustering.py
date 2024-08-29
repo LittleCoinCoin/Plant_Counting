149348fd64b764e572a3ef5bbfbc6579f4d4bf6d
Eliott <eliott.jacopin@laposte.net>
2024-08-29 15:44:24 +0900

    [Add] Prototyping with sklearn dbscan
    Major:
    - Toy example to reunite with my marks

---
+++ Pre_Treatments/Clustering.py
@@ -0,0 +1,41 @@
+import cv2
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+
+def DBSCANWorkflow(_image_path: str):
+    # Load the image
+    image = cv2.imread(_image_path)
+
+    # Take a subpart of the image in the center that is 100x100 pixels
+    # This is done to speed up the clustering
+    center_x = image.shape[1] // 2
+    center_y = image.shape[0] // 2
+    subpart = image[center_y - 50:center_y + 50, center_x - 50:center_x + 50]
+    # Keep only the first channel
+    subpart = subpart[:, :, 0]
+    reshaped_image = subpart.reshape(-1, 1)
+    print("subpart: ", subpart)
+
+    # Perform DBSCAN clustering
+    print("Performing DBSCAN clustering")
+    dbscan = DBSCAN(eps=3, min_samples=5)
+    print("Fitting")
+    clustering = dbscan.fit(reshaped_image)
+    print("Labels: ", clustering.labels_)
+
+    # Reshape the labels back into the original subpart shape
+    print("Reshaping")
+    clustered_subpart = clustering.labels_.reshape(subpart.shape)
+    print("Clustered subpart: ", clustered_subpart)
+
+    # Display the clustered subpart. Use matplotlib for this
+    print("Displaying")
+    #create a new figure
+    plt.figure()
+    #show the image
+    plt.imshow(clustered_subpart, cmap='viridis')
+    plt.show()
+
+if (__name__ == '__main__'):
+    image_path = "Tutorial/Output_General/Set1/Output/Session_1/Otsu/OTSU_rgb_83.jpg"
+    DBSCANWorkflow(image_path)
